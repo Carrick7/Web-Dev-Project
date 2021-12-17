8b3a11c3bb5078d81a39e7db881b4e3c780ddb99
cianrobrien <92682119+cianrobrien@users.noreply.github.com>
2021-12-17 18:23:10 +0000

    Java Script for the Privacy Page

---
+++ js/privacy_script.js
@@ -0,0 +1,51 @@
+function message(){
+	event.preventDefault();
+	document.getElementById("form").style.display = "none";
+	var userName=document.getElementById("formName").value;
+	var userEmail=document.getElementById("formEmail").value;
+	alert(userName+" , thank you for your Subject Access Request. We will send a copy of all relevant personal data to " +userEmail+ " within 30 days.");
+}
+
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } 
+  else {
+    text.style.display = "none";
+  }
+  
+}
+
+
+
+ function generateCode() {	
+
+	let x = Math.floor((Math.random() * 100000000) + 1);	
+	alert("Your code is "+x);
+
+}
+
+function learnMoreAds() {
+	var popup = document.getElementById("adsInfo");
+	popup.classList.toggle("show");
+		
+}	
+
+function learnMoreServices() {
+	var popup = document.getElementById("servicesInfo");
+	popup.classList.toggle("show");
+		
+}
+
+function learnMoreImprovements() {
+	var popup = document.getElementById("improvementsInfo");
+	popup.classList.toggle("show");
+		
+}	
